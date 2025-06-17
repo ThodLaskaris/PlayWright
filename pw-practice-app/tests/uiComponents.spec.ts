0fdcd73b9d09fad16c49b1eddf9cb1d0cd056b64
thodlaskaris <thodlas@icloud.com>
2025-06-17 15:59:17 +0300

    Lists Dropdowns and Css assertion

--- pw-practice-app/tests/uiComponents.spec.ts
+++ pw-practice-app/tests/uiComponents.spec.ts
@@ -48,7 +48,60 @@ test.describe('Form Layouts page', () => {
 
         await usingTheGridForm.getByRole('radio', { name: "Option 2" })
             .check({ force: true })
-            expect(await usingTheGridForm.getByRole('radio', {name: "Option 1"}).isChecked()).toBeFalsy()
-            expect(await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 1" }).isChecked()).toBeFalsy()
+        expect(await usingTheGridForm.getByRole('radio', { name: "Option 2" }).isChecked()).toBeTruthy()
     })
+})
+
+
+test('checkboxes', async ({ page }) => {
+
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+    await page.getByRole('checkbox', { name: "Hide on click" })
+        .click({ force: true }) // Check για το αν ειναι Checked και Click για click ( ανεξαρτητου τι status εχει ), uncheck για unchecked
+    await page.getByRole('checkbox', { name: "Prevent arising of duplicate toast" })
+        .check({ force: true })
+
+    const allBoxes = await page.getByRole('checkbox')
+    for (const box of await allBoxes.all()) { //.all() will create an array of the element
+        await box.uncheck({ force: true })
+        expect(await box.isChecked()).toBeFalsy()
+    }
+})
+
+test('lists and dropdowns', async ({ page }) => {
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list') // list can be used when has UL tag parent container for the list
+    page.getByRole('listitem') // when the list has LI tag all the list items
+
+    // const optionList = page.getByRole('list').locator('nb-option')
+    // more compact way would be ..
+    const optionList = page.locator('nb-option-list nb-option')
+
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({ hasText: "Cosmic" }).click()
+
+
+    // theme
+
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+    await dropDownMenu.click()
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if(color != "Corporate")
+        await dropDownMenu.click()
+    }
 })
